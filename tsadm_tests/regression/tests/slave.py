2981eef9a2de3cda35ba268bc47e8ab6b9d9937f
Jeremías <jeremias@tincan.co.uk>
2015-07-30 23:42:50 -0300

    remove old regression tests

--- tsadm_tests/regression/tests/slave.py
+++
@@ -1,8 +0,0 @@
-
-from .. import RegressionTest
-
-class RTSlave(RegressionTest):
-    def configure(self):
-        self.url = '/slave/1272428384/'
-        self.check_regex = r'^IP address: '
-        self.fail_abort = True
