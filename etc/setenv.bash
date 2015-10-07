9a95fc89e48e7a579f7b9f28096881f1ea1ba199
Jeremías <jeremias@tincan.co.uk>
2015-10-06 23:44:29 -0300

    script to import users ssh auth keys

--- etc/setenv.bash
+++ etc/setenv.bash
@@ -2,6 +2,7 @@ RUN_MODE=dev
 CODEDIR=/opt/tsadm${RUN_MODE}
 SITES_BASEDIR=/home/tsadm${RUN_MODE}/sites
 SITES_REPO_BASEDIR=/srv/git/tsadm${RUN_MODE}/sites
+TSADM_DBNAME=tsadm${RUN_MODE}db
 TSADM_HOMEDIR=/home/tsadm${RUN_MODE}
 TSADM_USER=tsadm${RUN_MODE}
 USERS_REPO_BASEDIR=/srv/git/tsadm${RUN_MODE}/users
