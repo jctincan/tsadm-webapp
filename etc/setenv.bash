a2782c5bbab62edcbace272ba9e1d9a3137a9f70
Jeremías <jeremias@tincan.co.uk>
2015-10-06 23:08:48 -0300

    use a central file for scripts settings

---
+++ etc/setenv.bash
@@ -0,0 +1,8 @@
+RUN_MODE=dev
+CODEDIR=/opt/tsadm${RUN_MODE}
+SITES_BASEDIR=/home/tsadm${RUN_MODE}/sites
+SITES_REPO_BASEDIR=/srv/git/tsadm${RUN_MODE}/sites
+TSADM_HOMEDIR=/home/tsadm${RUN_MODE}
+TSADM_USER=tsadm${RUN_MODE}
+USERS_REPO_BASEDIR=/srv/git/tsadm${RUN_MODE}/users
+WWW_GROUP=nobody
